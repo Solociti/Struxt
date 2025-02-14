0f1648e0a809cc46ee949a6ba2ca1f20fa0da394
Trevor Reimer <trevorreimer95@gmail.com>
2025-02-13 20:51:02 -0400

    Added knex migration to server start

--- server/apiEntry.ts
+++ server/apiEntry.ts
@@ -1,30 +1,39 @@
 import express from "express";
-
 import {
   router as assetsRouter,
   staticFiles as assetStaticFiles,
 } from "./api/assets/register";
 import { router as projectsRouter } from "./api/projects/register";
+import { knex } from "./utils/database";
 
-const app = express();
-const port = 3000;
+// migrate the database to latest version and then start the server
+console.log("Migrating database...");
+knex.migrate.latest().then(() => {
+  main();
+});
 
-// reduce fingerprinting
-app.disable("x-powered-by");
+async function main() {
+  console.log("Starting server...");
+  const app = express();
+  const port = 3000;
 
-app.use(express.json());
+  // reduce fingerprinting
+  app.disable("x-powered-by");
 
-app.get("/api", (req, res) => {
-  res.json({
-    now: new Date(),
+  app.use(express.json());
+
+  app.get("/api", (req, res) => {
+    res.json({
+      now: new Date(),
+    });
   });
-});
 
-app.use("/api/assets", assetsRouter);
-app.use("/assets", assetStaticFiles);
+  app.use("/api/assets", assetsRouter);
+  app.use("/assets", assetStaticFiles);
 
-app.use("/api/projects", projectsRouter);
+  app.use("/api/projects", projectsRouter);
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
+}
