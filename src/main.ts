f004bc7bd8dddee4f7d6be482e7f2c15e3908bac
Trevor Reimer <trevorreimer95@gmail.com>
2025-03-01 18:28:24 -0400

    Fixed build issues

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import { registerElements } from "./components/htmlElements";
 import { createChild } from "./createChild";
 import { addFonts } from "./fonts/addFonts";
 import { publishSite } from "./publish/publishSite";
+import { Editor } from "grapesjs";
+import { registerImageViewer } from "./components/imageViewer";
 
 import "./external-sites/site.scss";
 import "./style.css";
 // @ts-ignore
 import "@grapesjs/studio-sdk/style";
-import { registerImageViewer } from "./components/imageViewer";
 
 let projectId = location.hash.slice(1) || "default";
 
@@ -69,14 +70,20 @@ createStudioEditor({
           type: "canvasSidebarTop",
           sidebarTop: {
             leftContainer: {
-              buttons: ({ items }) => [
+              buttons: ({ items }: { items: any[] }) => [
                 ...items,
                 {
                   id: "",
                   type: "button",
                   icon: '<svg viewBox="0 0 24 24"><path d="m13.13 22.19-1.63-3.83a21.05 21.05 0 0 0 4.4-2.27l-2.77 6.1M5.64 12.5l-3.83-1.63 6.1-2.77a21.05 21.05 0 0 0-2.27 4.4M21.61 2.39S16.66.27 11 5.93a19.82 19.82 0 0 0-4.35 6.71c-.28.75-.09 1.57.46 2.13l2.13 2.12c.55.56 1.37.74 2.12.46A19.1 19.1 0 0 0 18.07 13c5.66-5.66 3.54-10.61 3.54-10.61m-7.07 7.07a2 2 0 0 1 2.83-2.83 2 2 0 0 1-2.83 2.83m-5.66 7.07-1.41-1.41 1.41 1.41M6.24 22l3.64-3.64a3.06 3.06 0 0 1-.97-.45L4.83 22h1.41M2 22h1.41l4.77-4.76-1.42-1.41L2 20.59V22m0-2.83 4.09-4.08c-.21-.3-.36-.62-.45-.97L2 17.76v1.41Z"/></svg>',
                   tooltip: "Publish website ",
-                  onClick: ({ editor, event }) => {
+                  onClick: ({
+                    editor,
+                    event,
+                  }: {
+                    editor: Editor;
+                    event: any;
+                  }) => {
                     const layoutId = "publishWebsiteProd";
                     const rect = event.currentTarget.getBoundingClientRect();
 
