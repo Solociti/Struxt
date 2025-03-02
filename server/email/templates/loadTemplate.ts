8547a73f54611f05a89de7511bdeaf81825684bb
Trevor Reimer <trevorreimer95@gmail.com>
2025-03-01 23:08:18 -0400

    Fixed a dirname problem

--- server/email/templates/loadTemplate.ts
+++ server/email/templates/loadTemplate.ts
@@ -1,5 +1,7 @@
+import { getDirname } from "cross-dirname";
 import Handlebars from "handlebars";
 import { readFile } from "node:fs/promises";
+import { join } from "node:path";
 
 /**
  * Load the given html template file
@@ -8,7 +10,8 @@ import { readFile } from "node:fs/promises";
  * @returns
  */
 export async function loadTemplate(name: string) {
-  const html = await readFile(`server/email/templates/${name}.html`, "utf-8");
+  const dir = getDirname();
+  const html = await readFile(join(dir, `/${name}.html`), "utf-8");
 
   const template = Handlebars.compile(html);
   return template;
